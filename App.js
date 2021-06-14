e8a37f236ce4bb36188cfc4303887ad63e7eb956
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-06-14 19:12:04 -0300

    Fixed: code clean, file clean, update package.json

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import {ScrollView, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CustomImage from './src/components/screens/CustomImage'
 import regigigas from './assets/486.png';
-import basculin from './assets/550.svg';
 import Styles from "./src/Styles";
 import {StatusBar} from "expo-status-bar";
 
 const heatran = require('./assets/485.png');
 const giratina = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/487.png";
-const hydration = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/489.svg";
+// const hydration = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/489.svg";
 
 export default function App() {
     return (
@@ -19,24 +18,16 @@ export default function App() {
                 <Text style={Styles.tituloView}>
                     Três formas de inserir imagens
                 </Text>
-                <CustomImage fromWeb={false} isSvg={false} image={heatran}
+                <CustomImage fromWeb={false} image={heatran}
                              title={"Imagem local do Pokémon Heatran com require"}
                              width={220}
                              height={170}/>
-                <CustomImage fromWeb={false} isSvg={false} image={regigigas}
+                <CustomImage fromWeb={false} image={regigigas}
                              title={"Imagem local do Pokémon Regigigas com import"}
                              width={220}
                              height={170}/>
-                <CustomImage fromWeb={true} isSvg={false} image={giratina}
+                <CustomImage fromWeb={true} image={giratina}
                              title={"Imagem do Pokémon Giratina com uri"}
-                             width={220}
-                             height={170}/>
-                <CustomImage fromWeb={false} isSvg={true} image={basculin}
-                             title={"Imagem local do Pokémon Hydration em svg"}
-                             width={220}
-                             height={170}/>
-                <CustomImage fromWeb={true} isSvg={true} image={hydration}
-                             title={"Imagem do Pokémon Hydration com uri em svg"}
                              width={220}
                              height={170}/>
             </ScrollView>
